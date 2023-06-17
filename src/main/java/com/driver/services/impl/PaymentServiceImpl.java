4a1e992d59db9e06642ee1b4688dac150435ef27
ranjeetmehta <ranjeetkumarmehta14418@gmail.com>
2023-06-17 22:31:19 +0530

    done2

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -1,108 +1,50 @@
 package com.driver.services.impl;
 
-import com.driver.model.ParkingLot;
-import com.driver.model.Spot;
-import com.driver.model.SpotType;
-import com.driver.repository.ParkingLotRepository;
-import com.driver.repository.SpotRepository;
-import com.driver.services.ParkingLotService;
+import com.driver.model.Payment;
+import com.driver.model.PaymentMode;
+import com.driver.model.Reservation;
+import com.driver.repository.PaymentRepository;
+import com.driver.repository.ReservationRepository;
+import com.driver.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
-public class ParkingLotServiceImpl implements ParkingLotService {
+public class PaymentServiceImpl implements PaymentService {
     @Autowired
-    ParkingLotRepository parkingLotRepository1;
+    ReservationRepository reservationRepository2;
     @Autowired
-    SpotRepository spotRepository1;
-    @Override
-    public ParkingLot addParkingLot(String name, String address) {
-        ParkingLot newParkingLot=new ParkingLot();
-        newParkingLot.setName(name);
-        newParkingLot.setAddress(address);
-
-        parkingLotRepository1.save(newParkingLot);
-
-        return newParkingLot;
-
-    }
+    PaymentRepository paymentRepository2;
 
     @Override
-    public Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour)  {
-        ParkingLot newParkingLot;
+    public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
+        Reservation reservation;
+        try {
+            reservation = reservationRepository2.findById(reservationId).get();
 
-        newParkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        }catch (Exception e){
+            throw new RuntimeException();
+        }
+        Payment payment = new Payment();
 
+        // amount check and mode check
+        int bill = reservation.getSpot().getPricePerHour() * reservation.getNumberOfHours();
 
-        Spot newSpot = new Spot();
-        SpotType st;
-        if(numberOfWheels<=2){
-            st=SpotType.TWO_WHEELER;
+        if(amountSent < bill){
+            throw new Exception("Insufficient Amount");
+        }
+        if (!mode.toUpperCase().equals("CASH") && !mode.toUpperCase().equals("CARD") && !mode.toUpperCase().equals("UPI") ) {
+            throw new Exception("Payment mode not detected");
         }
 
-        else if (numberOfWheels==4 || numberOfWheels==3) {
-            st=SpotType.FOUR_WHEELER;
-
-        }
-        else st=SpotType.OTHERS;
-
-        newSpot.setSpotType(st);
-        newSpot.setPricePerHour(pricePerHour);
-
-        newParkingLot.getSpotList().add(newSpot);
-
-        parkingLotRepository1.save(newParkingLot);
-
-        newSpot.setParkingLot(newParkingLot);
-
-//        spotRepository1.save(newSpot);
-
-        return newSpot;
+        PaymentMode paymentMode = PaymentMode.valueOf(mode.toUpperCase());
+        payment.setPaymentMode(paymentMode);
+        payment.setPaymentCompleted(true);
+        payment.setReservation(reservation);
+        reservation.setPayment(payment);
 
 
-    }
-
-    @Override
-    public void deleteSpot(int spotId)  {
-
-        spotRepository1.deleteById(spotId);
-
-
-
-    }
-
-    @Override
-    public Spot updateSpot(int parkingLotId, int spotId, int pricePerHour) {
-        ParkingLot parkingLot;
-
-        parkingLot=parkingLotRepository1.findById(parkingLotId).get();
-
-
-
-        List<Spot> spots = parkingLot.getSpotList();
-
-        Spot updatedSpot = null;
-        for(Spot s : spots){
-            if(s.getId() == spotId){
-                s.setPricePerHour(pricePerHour);
-                updatedSpot = s;
-                spotRepository1.save(updatedSpot);
-                break;
-            }
-        }
-
-        return updatedSpot;
-
-    }
-
-    @Override
-    public void deleteParkingLot(int parkingLotId) {
-
-        parkingLotRepository1.deleteById(parkingLotId);
-
-
+        reservationRepository2.save(reservation);
+        return payment;
     }
 }
